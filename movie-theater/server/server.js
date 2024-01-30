e6f2958391d592589570038a0b7c56436c24d3f7
han0224 <dbn0224@gmail.com>
2024-01-30 17:02:18 +0900

    server 수정
    - movie name을 관리하는 상수생성
    - 좌석 상태를 나타내는 상수 생성
    => 직접 작성하다보니 오타로 인해 문제 발생해서 상수 생성
    - 좌석 정보 깊은 복사로 수정
    => 얕은 복사로 진행해 각 영화별 좌석이 제대로 변경 안되는 문제 발생했었음
    - setSeats 함수 변경
    => 취소, 확정 둘다 변경할 수 있도록 수정

--- movie-theater/server/server.js
+++ movie-theater/server/server.js
@@ -8,53 +8,62 @@ const io = new Server("5000", {
   },
 });
 
-let avatar = [...seats];
-let antman = [...seats];
-let cats = [...seats];
+const avatar = seats.map((v) => [...v]);
+const antman = seats.map((v) => [...v]);
+const cats = seats.map((v) => [...v]);
 
-const setSeats = (roomNumber, seat) => {
-  let temp = [];
-  const setStatus = (seats) => {
-    return seats.map((i) => {
-      let temp = { ...i };
-      if (i.seatNumber === seat) {
-        temp = { ...i, status: 3 };
-      }
-      return temp;
-    });
-  };
-  if (roomNumber === 3) {
-    temp = [...avatar].map((s) => setStatus(s));
-    avatar = [...temp];
-  } else if (roomNumber === 2) {
-    temp = [...antman].map((s) => setStatus(s));
-    antman = [...temp];
-  } else {
-    temp = [...cats].map((s) => setStatus(s));
-    cats = [...temp];
+const MOVIE_NAME = {
+  1: avatar,
+  2: antman,
+  3: cats,
+};
+const SEAT_STATUS = {
+  CONFIRM: 3,
+  CANCEL: 1,
+};
+/**
+ * seats: 현재 선택된 영화 좌석들
+ * seat: 상태를 변경할 좌석
+ * status: 취소(1) 또는 확정(3)
+ */
+const updateSeat = (seats, seat, status) => {
+  for (let i = 0; i < seats.length; i++) {
+    const index = seats[i].findIndex((v) => v.seatNumber === seat);
+    if (index < 0) continue;
+    seats[i][index] = { ...seats[i][index], status };
+    return seats;
   }
+  return null;
+};
+
+const setSeats = (roomNumber, seat, status) => {
+  const temp = updateSeat(MOVIE_NAME[roomNumber], seat, status);
+  if (!temp) new Error("err");
+
   return temp;
 };
 
 io.on("connection", (socket) => {
   socket.on("join", (movie) => {
     socket.join(movie);
-    let tempSeat = [];
-    if (movie === "1") {
-      tempSeat = avatar;
-    } else if (movie === "2") {
-      tempSeat = antman;
-    } else {
-      tempSeat = cats;
-    }
+
+    const tempSeat = MOVIE_NAME[movie];
     io.sockets.in(movie).emit("sSeatMessage", tempSeat);
   });
 
   socket.on("addSeat", (seat) => {
     const myRooms = Array.from(socket.rooms);
-    io.sockets.in(myRooms[1]).emit("sSeatMessage", setSeats(myRooms[1], seat));
+    io.sockets
+      .in(myRooms[1])
+      .emit("sSeatMessage", setSeats(myRooms[1], seat, SEAT_STATUS.CONFIRM));
   });
 
+  socket.on("cancelSeat", (seat) => {
+    const myRooms = Array.from(socket.rooms);
+    io.sockets
+      .in(myRooms[1])
+      .emit("sSeatMessage", setSeats(myRooms[1], seat, SEAT_STATUS.CANCEL));
+  });
   socket.on("disconnection", () => {
     console.log("logout");
   });
